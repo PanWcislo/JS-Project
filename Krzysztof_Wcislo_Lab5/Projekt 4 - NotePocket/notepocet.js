fa37e2b64f5617c8201d78ad2b5456c4198c5350
Krzysztof Wcislo <krzysztofwcislo1234@gmail.com>
2019-03-04 00:36:00 +0100

    zmiany notes

--- Krzysztof_Wcislo_Lab5/Projekt 4 - NotePocket/notepocet.js
+++ Krzysztof_Wcislo_Lab5/Projekt 4 - NotePocket/notepocet.js
@@ -20,9 +20,9 @@ window.addEventListener('load',(e)=>{
             let title = document.querySelector("#title").value
             let content = document.querySelector("#content").value
             let color = document.querySelector("#color").value
-            let priority = document.querySelector("#attached").checked
+            let attached = document.querySelector("#attached").checked
 
-            AddNote(title,content,color,priority)
+            AddNote(title,content,color,attached)
         })
 
         if(notes.length > 0){
@@ -35,11 +35,12 @@ window.addEventListener('load',(e)=>{
     }
 })
 
-function AddNote(title,content,color,priority){
+function AddNote(title,content,color,attached){
     notes.push(
-        new Note(title,content,color,priority)
+        new Note(title,content,color,attached)
     )
 
+    notes.sort(sortNotes)
     showNotes()
 
     let notesJSON = JSON.stringify(notes)
@@ -52,7 +53,11 @@ function showNotes(){
 
     notes.forEach((elem)=>{
         let card = document.createElement("div")
-        card.className="note";
+        card.className="note1";
+
+        let attached1 = document.createElement('div')
+        attached1.className = "note_attached"
+        attached1.textContent = "Attached"
 
         let content1 = document.createElement("div")
         content1.className = "note_title"
@@ -70,18 +75,36 @@ function showNotes(){
         let feed = document.createElement("div")
         feed.className="note_text"
         feed.textContent = elem.content
+        feed.style.color = elem.color
 
         content2.appendChild(feed)
 
         let date = document.createElement("div")
         date.className="note_footer"
         date.textContent += new Date(elem.creationDate).toLocaleString()
-
+        
         card.appendChild(content1)
         card.appendChild(content2)
         card.appendChild(date)
         list.appendChild(card)
     })
+}
+
+function sortNotes(n1,n2){
+
+
+    if(n1.attached == true && n2.attached == false)
+            return -1
+
+        if(n1.attached == false && n2.attached == true)
+            return 1
+
+        if( new Date(n1.createdTime).getTime() > new Date(n2.createdTime).getTime() )
+            return -1
+        if( new Date(n1.createdTime).getTime() < new Date(n2.createdTime).getTime() )
+            return 1
+
+        return 0
 }
 
 class Note{
@@ -92,4 +115,5 @@ class Note{
         this.priority = _priority;
         this.creationDate = new Date()
     }
+
 }
